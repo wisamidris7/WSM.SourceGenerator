7dc0f8a9371ad3088a5b786305a3f25e519019b4
Wisam idris <wisamidris7@gmail.com>
2023-06-03 16:31:56 -0700

    Update In Config

--- src/WSM.SourceGenerator.Gen/Config/GenerationConfig.cs
+++ src/WSM.SourceGenerator.Gen/Config/GenerationConfig.cs
@@ -7,20 +7,17 @@ namespace WSM.SourceGenerator.Gen.Config
     public class GenerationConfig
     {
         public GenerationConfigClass Class { get; set; }
-        public GenerationConfigApiClient Client { get; set; }
+        public GenerationConfigApiClient Api { get; set; }
+        public string? Namespace { get; set; } = "{name}Controller";
     }
     public class GenerationConfigApiClient
     {
-        public string ControllersPath { get; set; }
-        public string MethodFormat { get; set; }
-        public string ServiceFormat { get; set; }
-        public string InterfaceFormat { get; set; }
+        public string? ControllerName { get; set; } = "{name}Controller";
     }
     public class GenerationConfigClass
     {
         public string? GenerateDtoAttribute { get; set; }
         public string? GenerateOptionalAttribute { get; set; }
-        public string? Namespace { get; set; }
         public bool GenerateEmptyConstructor { get; set; }
         public bool GenerateFieldsConstructor { get; set; }
         public bool MakePrivateAndPublicFields { get; set; }
@@ -31,6 +28,5 @@ namespace WSM.SourceGenerator.Gen.Config
         public bool NullableSupport { get; set; }
         public bool OneGeneratorFile { get; set; }
         public string? OneGeneratorFileName { get; set; }
-
     }
 }
