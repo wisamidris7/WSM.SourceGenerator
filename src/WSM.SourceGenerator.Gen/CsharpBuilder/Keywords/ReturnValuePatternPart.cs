2b7f0202e33802678933fa7e8905afee3eb6aeb3
Wisam idris <wisamidris7@gmail.com>
2023-06-03 16:46:50 -0700

    Create ReturnValuePatternPart.cs

---
+++ src/WSM.SourceGenerator.Gen/CsharpBuilder/Keywords/ReturnValuePatternPart.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WSM.SourceGenerator.Gen.CsharpBuilder.Keywords
+{
+    public class ReturnValuePatternPart : ICSBuilderPart
+    {
+        public ReturnValuePatternPart(string value, bool oneLine=false)
+        {
+            Value = value;
+            OneLine = oneLine;
+        }
+        public ReturnValuePatternPart(ICSBuilder operation, bool oneLine = false)
+        {
+            Value = operation.Build().ToString();
+            OneLine = oneLine;
+        }
+
+        public string Value { get; }
+        public bool OneLine { get; }
+
+        public StringBuilder Build(StringBuilder builder)
+        {
+            if (OneLine) return builder.Append($" => {Value}");
+
+            builder.Append($"return {Value}");
+        }
+    }
+}
